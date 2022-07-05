86a80a6a3095bd28487123c0397dff0e60d90b95
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-05 21:23:08 +0800

    add first practice

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hai")
+const x = "Apa Kabar";
+
+console.log(x.length);
+console.log(x.indexOf('Dunia'));
+console.log(x.substr(5,3));
+console.log(x.slice(5,8));
+console.log(x.split(' '));
+
+const a = 'Apa';
+const b = 'KABAR';
+const c = 12345;
+
+console.log(a.toUpperCase());
+console.log(b.toLowerCase());
+console.log(a.replace('pa','mi'));
+console.log(a.replace(/pa/g, 'mi'));
+console.log(typeof(c.toString()));
