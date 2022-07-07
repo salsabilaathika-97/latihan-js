706cc437da2a5294b5180eadf8279687dcfbd2b6
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-07 21:53:51 +0800

    add practice if condition

--- practice.js
+++ practice.js
@@ -2,6 +2,7 @@
 
 const score = true;
 const attendance = false;
+const attitude = false;
 
 const result1 = score || attendance;
 const result2 = score && attendance;
@@ -12,3 +13,30 @@ console.log(result1);
 console.log(result2);
 console.log(result3);
 console.log(result4);
+
+// If Condition
+
+const total_score = false;
+const total_attendance = false;
+
+if (total_score && total_attendance) {
+    console.log("Lulus");
+} else if (total_score && !total_attendance) {
+    console.log("Lulus Bersyarat");
+} else {
+    console.log("Tidak Lulus");
+}
+
+const number = 15;
+const Condition1 = number % 5 == 0;
+const Condition2 = number % 3 == 0;
+
+if (Condition1 && Condition2) {
+    console.log("Fizz Buzz")
+} else if (number % 3 == 0 ) {
+    console.log("Fizz")
+} else if (number % 5 == 0) {
+    console.log("Buzz")
+} else {
+    console.log(number);
+}
