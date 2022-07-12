1edc3d5b1271e86f6d1cf2c173b39521091f7fda
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-12 22:55:01 +0800

    add practice looping for 4

--- practice-2.js
+++ practice-2.js
@@ -49,3 +49,73 @@ const removeCayla = function(arr) {
 
 const hasil_3 = removeCayla(car2);
 console.log(hasil_3);
+
+const fruits = ['Apple', 'Orange', 'Peach'];
+
+const newFruits = [];
+
+for (const fruit of fruits) {
+    if (fruit !== 'Apple'){
+        newFruits.push(fruit);
+    }
+}
+
+console.log(newFruits);
+
+
+
+const Remove = function () {
+    const scores = [100, 90, 70, 80];
+    const newData =  [];
+    for (score of scores) {
+        if (score < 90 && score > 60){
+            newData.push(score);
+        }
+    }
+    return newData;
+}
+
+const hasil_4 = Remove();
+console.log(hasil_4);
+
+const kelas1 = [100, 90, 70, 80];
+
+const between = function(nilaiSiswa){
+    let newData = [];
+    for (nilai of nilaiSiswa){
+        if (nilai > 60 && nilai < 90){
+            newData.push(nilai);
+        }
+    }
+    return newData;
+}
+
+const nilai1 = between(kelas1);
+console.log(nilai1);
+
+const kelas2 = [100, 90, 80];
+
+let newData = [];
+
+kelas2.forEach((nilai, index) => {
+    newData.push(`Anak ke ${index + 1} nilainya adalah ${nilai}`)
+});
+
+console.log(newData);
+
+const warga = [1991, 1992, 1875, 2004];
+
+function cekUsia(data){
+    let dataWarga = [];
+    data.forEach((usia) => {
+        dataWarga.push(`Usia kelahiran ${usia} tahun ini adalah ${2022 - usia}`)
+    });
+    return  dataWarga;
+}
+
+const hasil_5 = cekUsia(warga);
+console.log(hasil_5);
+
+
+
+
