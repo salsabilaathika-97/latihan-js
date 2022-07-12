08faddae4dfa1b526e810c8d691763fd384ad67a
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-12 21:50:27 +0800

    add practice looping for 3

--- practice-2.js
+++ practice-2.js
@@ -22,3 +22,30 @@ const multiple = (arr) => {
 
 const hasil_1 = multiple(number);
 console.log(hasil_1);
+
+const car = ['Agya', 'Ayla', 'Sigra', 'Cayla'];
+
+function substract(arr, value) {
+    for (let i = 0; i<arr.length;i++){
+        const splice = arr.splice(arr[i], value);
+        return splice;
+    }
+}
+
+const hasil_2 = substract(car, 3);
+console.log(hasil_2);
+
+const car2 = ['Cayla', 'Agya', 'Ayla', 'Etios', 'Terios'];
+
+const removeCayla = function(arr) {
+    let newData = [];
+    for(let i = 0; i<arr.length;i++){
+        if(arr[i] !== "Cayla"){
+            newData.push(arr[i]);
+        }
+    }
+    return newData;
+};
+
+const hasil_3 = removeCayla(car2);
+console.log(hasil_3);
