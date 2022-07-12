b107f2ab2a45408bb12b7bc9750f38275dac0812
salsabilaathika-97 <salsathikar@gmail.com>
2022-07-12 21:13:32 +0800

    add practice looping for 2

--- practice-2.js
+++ practice-2.js
@@ -2,12 +2,23 @@
 
 let number = [100, 200, 300, 400, 500]
 
-function multiply (number) {
-    for (let x=0; x < number.length ; x++) {
-       number[x] *= 2;
+// function multiply (number) {
+//     for (let x=0; x < number.length ; x++) {
+//        number[x] *= 2;
+//     }
+//     return number;
+// }
+
+// console.log(number);
+// console.log(multiply(number));
+
+const multiple = (arr) => {
+    let newData = []
+    for (let i=0; i < arr.length ; i++){
+        newData.push(arr[i] * 2);
     }
-    return number;
+    return newData;
 }
 
-console.log(number);
-console.log(multiply(number));
+const hasil_1 = multiple(number);
+console.log(hasil_1);
